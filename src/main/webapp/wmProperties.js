3e78b94bc078316badcc02d0c642aa91276b72c0
root <root@d3a2e0704417>
2017-06-14 18:05:13 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_iMKVRVWDfz",
+  "homePage" : "Main",
+  "name" : "Auto_iMKVRVWDfz",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
